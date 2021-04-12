552c054f8f48cf67715c8c2a502e65e10064ed8d
WilsonSalinasFlores <63874104+WilsonSalinasFlores@users.noreply.github.com>
2021-04-12 10:32:10 -0500

    Primer Commit

---
+++ src/test/java/com/example/pocfirestoregcp/PocFireStoreGcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pocfirestoregcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocFireStoreGcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
