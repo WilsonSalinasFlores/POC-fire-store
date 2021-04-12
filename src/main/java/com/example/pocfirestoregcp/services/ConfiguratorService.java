de30e463a3869cb184faf5c371da86fd3c4a41df
WilsonSalinasFlores <63874104+WilsonSalinasFlores@users.noreply.github.com>
2021-04-12 18:29:13 -0500

    Rest Controler Nuevo

--- src/main/java/com/example/pocfirestoregcp/services/ConfiguratorService.java
+++
@@ -1,9 +0,0 @@
-package services;
-
-import com.example.pocfirestoregcp.pojo.ConfigurationPojo;
-
-import java.util.concurrent.ExecutionException;
-
-public interface ConfiguratorService {
-    ConfigurationPojo insert(final ConfigurationPojo configurationPojo) throws ExecutionException, InterruptedException;
-}
